17ccb6e7a28b2987a0af964df7e27a800d4a7f19
sdcaulley <stephanie.caulley@gmail.com>
2017-02-03 05:18:49 -0800

    server running, GET and POST responding as programmed

--- test/server.test.js
+++ test/server.test.js
@@ -1,11 +1,28 @@
 const chai = require('chai');
-const assert = require('chai').assert;
+const assert = chai.assert;
 const http = require('chai-http');
+const server = require('../lib/http-server');
 
 chai.use(http);
 
 describe('basic server function', () => {
-    it('server starts', done => {
+    const request = chai.request(server);
 
+    it('GET / say "helllo world"', done => {
+        request
+            .get('/')
+            .end((err, res) => {
+                assert.strictEqual(res.text, 'hello world!');
+                done();
+            });
+    });
+
+    it('POST says 404 not supported', done => {
+        request
+            .post('/')
+            .end((err, res) => {
+                assert.strictEqual(res.text, 'CANNOT POST at /');
+                done();
+            });
     });
 });
