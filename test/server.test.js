7e6939ac936cf358195106af98d3d09e090521d5
sdcaulley <stephanie.caulley@gmail.com>
2017-02-02 16:31:58 -0800

    basic server running, receiving GET req

---
+++ test/server.test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const assert = require('chai').assert;
+const http = require('chai-http');
+
+chai.use(http);
+
+describe('basic server function', () => {
+    it('server starts', done => {
+
+    });
+});
