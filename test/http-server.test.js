c39e0d14f7f3a7967117b46fb082293caa9c25af
Clarefollett <chlaw101@gmail.com>
2017-02-06 20:31:07 -0800

    I tried the same syntax logic for each of the tests. They all pass and fail. Some were failing and I didn't know why but fixed it pretty quickly

--- test/http-server.test.js
+++ test/http-server.test.js
@@ -18,10 +18,34 @@ describe('testing the http server that I made', () => {
     });
     it('gives a salutation with a name', done => {
         request
-        .get('/greeting/claire?salutation=hello')
-        .end((err, response) => {
-            assert.strictEqual(response.text, 'hello claire')
-            done();
+            .get('/greeting/claire?salutation=hello')
+            .end((err, response) => {
+                assert.strictEqual(response.text, 'hello claire')
+                done();
+        });
+    });
+    it('gives a salutation with no name', done => {
+        request
+            .get('/greeting?salutation=hey')
+            .end((err, response) => {
+                assert.strictEqual(response.text, 'hey stranger')
+                done();
         })
     })
+    it('says greeting with name', done => {
+        request
+            .get('/greeting/claire')
+            .end((err, response) => {
+                assert.strictEqual(response.text, 'hello claire')
+                done();
+        });
+    });
+    it('says greeting without name', done => {
+        request
+            .get('/greeting')
+            .end((err, response) => {
+                assert.strictEqual(response.text, 'hello stranger')
+                done();
+        });
+    });
 });
